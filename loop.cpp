9dde8f8f0864558c67bfeceaa0d206de1d803e67
Adarsh <Adarsh0278@users.noreply.github.com>
2026-04-19 02:36:30 +0530

    pattern

---
+++ loop.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// int main() {
+//     for (int i=0; i <= 5; i++) {
+//         // cout << "*";
+//         for(int j = 1; j <= i; j++) {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+// int main()
+// {
+//     int n = 6; // size of the row
+//     int m = 4; // size of the column
+//     for (int i = 0; i <= n; i++)
+//     {
+//         for (int j = 0; j <= m; j++)
+//         {
+//             if (i == 0 || i == n)   // top and bottom rows
+//             {
+//                 cout << "*";
+//             }
+//             else
+//             {
+//                 if (j == 0 || j == m)  // first and last column
+//                     cout << "*";
+//                 else
+//                     cout << " ";
+//             }
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// 1
+// 12
+// 123
+// 1234
+
+// int main() {
+//    for(int i = 1; i <= 4; i++) {
+//        for(int j = 1; j <= i; j++) {
+//            cout << j;
+//        }
+//        cout << endl;
+//    }
+//    return 0;
+// }
+
+
+int main() {
+    int n = 5; // size of the row
+    if (cout << n) {
+        cout << n << " is the size of the row." << endl;
+    }
+}
