1e2d85ff3599919541d82b78b05afe6c5896338f
Adarsh <Adarsh0278@users.noreply.github.com>
2026-06-10 14:00:14 +0530

    solve some question

---
+++ reverseArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//-------------------------------------
+// reverse an array
+
+void reverseArray(int arr[], int size){
+    int start = 0;
+    int end = size-1;
+    while (start < end)
+    {
+
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    
+}
+
+main()
+{
+    int arr[6] = {1, 2, 3, 4, 5,6};
+    int size = 6;
+
+    reverseArray(arr,size);
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
