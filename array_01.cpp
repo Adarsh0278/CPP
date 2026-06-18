61ca4d4228aedae39b3c7fa007b3379b869c9ddc
Adarsh <Adarsh0278@users.noreply.github.com>
2026-06-18 13:33:04 +0530

    solve array qestion

--- array_01.cpp
+++ array_01.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-
+#include <climits>
 // --------------------------------------------------
-//question = [-1,0,3,6,-4,43]
+// question = [-1,0,3,6,-4,43]
 // move all negative numbers to the beginning = [-1,-4,0,3,6,43]
 
 // void negativeArray(int arry[6], int size){
@@ -14,11 +13,11 @@ using namespace std;
 //         if(arry[i] < 0){
 //             swap(arry[i],arry[movingJ]);
 //             movingJ++;
-            
+
 //         }
-        
+
 //     }
-// } 
+// }
 
 // int main(){
 //     int arry[6]= {-1,0,3,6,-4,43} ;
@@ -31,14 +30,12 @@ using namespace std;
 //     return 0;
 // }
 
-
 //---------------------------------------------------------------
 
 // Move all zeros to the end
 
 // Input:  [0, 1, 0, 3, 12]
 // Output: [1, 3, 12, 0, 0]
-
 
 // void moveZoroEnd(int ary[5], int sz){
 //     int j=0;
@@ -91,15 +88,12 @@ using namespace std;
 //     return 0;
 // }
 
-
 // --------------------------------------------------------------------
 
 // Sort an array of 0s, 1s, and 2s (Dutch National Flag)
 
 // Input:  [2, 0, 2, 1, 1, 0]
 // Output: [0, 0, 1, 1, 2, 2]
-
-
 
 // void sortArray(vector<int>& arr){
 //     int left=0;
@@ -137,28 +131,128 @@ using namespace std;
 
 /// -----------------------------------------
 
-
-
 // k=2
 // input = [10,20,30,40,50,60]
 // output = [50,60,40,10,20,30]
 
-void rotation(vector<int>&arr, int k){
-    int n = arr.size();
-    vector<int> ans(n);
+// void rotation(vector<int>&arr, int k){
+//     int n = arr.size();
+//     vector<int> ans(n);
 
-    for(int index=0; index<n; index++){
-        int newIndex= (index + k)%n;
-        ans[newIndex] = arr[index];
+//     for(int index=0; index<n; index++){
+//         int newIndex= (index + k)%n;
+//         ans[newIndex] = arr[index];
+//     }
+//     arr= ans;
+
+// }
+// int main(){
+//     vector<int> arr= {10,20,30,40,50,60};
+//     int k=2;
+//     rotation(arr,k);
+//     for(int x:arr){
+//         cout<<x<<" ";
+//     }
+// }
+
+///---------------------------
+
+// [10,20,30,40,50]
+// [30,40,50,10,20]
+// (i+n)%n
+
+// void rotate(vector<int> &arr, int k)
+// {
+//     int n = arr.size();
+//     vector<int> ans(n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int newIndex = (i - k +n) % n;
+//         ans[newIndex] = arr[i];
+//     }
+//     arr = ans;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {1, 2, 3, 4, 5, 6};
+//     int k = 2;
+//     rotate(arr, k);
+//     for (int x : arr)
+//     {
+//         cout << x << " ";
+//     }
+//     return 0;
+// }
+
+
+//-------------------------------------------------------------------------------------
+
+// missing number
+// input: [3,0,1]
+// output: [2]
+// using formula ((n)*(n+1))/2
+
+// void missingNumber(vector<int>&arr){
+//     int n = arr.size();
+//     int sum =0;
+//     for(int i=0;i<n;i++){
+//         sum +=arr[i];
+//     }
+//     int total = ((n)*(n+1))/2;
+//     int missNum = total-sum;
+//     cout<<missNum;
+
+// }
+// int main(){
+//     vector<int> arr= {0,1,2,4};
+//     missingNumber(arr);
+//     return 0;
+// }
+
+
+//-------------------------------------------------------------------
+
+
+vector<int> rowMax(vector<vector<int>>& arr){
+    vector<int> ans;
+    int onecount = INT_MIN;
+    int row = -1;
+
+    for(int i = 0; i < arr.size(); i++){
+        int count = 0;
+
+        for(int j = 0; j < arr[i].size(); j++){
+            if(arr[i][j] == 1){
+                count++;
+            }
+        }
+
+        if(count > onecount){
+            onecount = count;
+            row = i;
+        }
     }
-    arr= ans;
-    
+
+    ans.push_back(row);
+    ans.push_back(onecount);
+
+    return ans;
 }
+
 int main(){
-    vector<int> arr= {10,20,30,40,50,60};
-    int k=2;
-    rotation(arr,k);
-    for(int x:arr){
-        cout<<x<<" ";
-    }
+    vector<vector<int>> arr = {
+        {1,0,0,0},
+        {0,1,1,0},
+        {1,1,1,0},
+        {0,0,1,0}
+    };
+
+    vector<int> result = rowMax(arr);
+
+    cout << "Row Index: " << result[0] << endl;
+    cout << "Number of 1s: " << result[1] << endl;
+
+    return 0;
 }
